671eac585868ce88ebc41e9c8202491d9a803658
alexv <alexv@gismeteo.ru>
2016-04-29 18:42:00 +0300

    first commit

---
+++ FMX.WebBrowserEx.pas
@@ -0,0 +1,328 @@
+(*
+ * TWebBrowserEx Classes
+ *   WebBrowser Componet for FireMonkey.
+ *
+ * Copyright (c) 2013, 2015 HOSOKAWA Jun.
+ *
+ * Last Update 2015/10/29
+ *
+ * Platform:
+ *   Windows, OS X, iOS, Android
+ *   Delphi / C++Builder XE5, XE6, XE7, XE8, 10 seattle
+ *   Appmethod 1.14, 1.15, 1.16, 1.17
+ *
+ * Contact:
+ *   Twitter @pik or freeonterminate@gmail.com
+ *
+ * Original Source:
+ *   https://github.com/freeonterminate/delphi/tree/master/TWebBrowser
+ *
+ * How to Use:
+ *   1. Add FMX.WebBroserEx to "uses" block.
+ *
+ *   2. Create WebBroserEx and Set parent.
+ *      procedure TForm1.FormCreate(Sender: TObject);
+ *      begin
+ *        FWebBrowser := TWebBrowserEx.Create(Self);
+ *        FWebBrowser.Parent := Panel1;
+ *        FWebBrowser.Align := TAlignLayout.Client;
+ *      end;
+ *
+ *   3. Set URL property.
+ *      procedure TForm1.Button1Click(Sender: TObject);
+ *      begin
+ *        FWebBrowser.URL := 'https://twitter.com/pik';
+ *      end;
+ *
+ * LICENSE:
+ *   本ソフトウェアは「現状のまま」で、明示であるか暗黙であるかを問わず、
+ *   何らの保証もなく提供されます。
+ *   本ソフトウェアの使用によって生じるいかなる損害についても、
+ *   作者は一切の責任を負わないものとします。
+ *
+ *   以下の制限に従う限り、商用アプリケーションを含めて、本ソフトウェアを
+ *   任意の目的に使用し、自由に改変して再頒布することをすべての人に許可します。
+ *
+ *   1. 本ソフトウェアの出自について虚偽の表示をしてはなりません。
+ *      あなたがオリジナルのソフトウェアを作成したと主張してはなりません。
+ *      あなたが本ソフトウェアを製品内で使用する場合、製品の文書に謝辞を入れて
+ *      いただければ幸いですが、必須ではありません。
+ *
+ *   2. ソースを変更した場合は、そのことを明示しなければなりません。
+ *      オリジナルのソフトウェアであるという虚偽の表示をしてはなりません。
+ *
+ *   3. ソースの頒布物から、この表示を削除したり、表示の内容を変更したりしては
+ *      なりません。
+ *
+ *   This software is provided 'as-is', without any express or implied warranty.
+ *   In no event will the authors be held liable for any damages arising from
+ *   the use of this software.
+ *
+ *   Permission is granted to anyone to use this software for any purpose,
+ *   including commercial applications, and to alter it and redistribute
+ *   it freely, subject to the following restrictions:
+ *
+ *   1. The origin of this software must not be misrepresented;
+ *      you must not claim that you wrote the original software.
+ *      If you use this software in a product, an acknowledgment in the product
+ *      documentation would be appreciated but is not required.
+ *
+ *   2. Altered source versions must be plainly marked as such,
+ *      and must not be misrepresented as being the original software.
+ *
+ *   3. This notice may not be removed or altered from any source distribution.
+ *)
+
+unit FMX.WebBrowserEx;
+
+interface
+
+uses
+  System.Classes
+  ,FMX.Types, FMX.WebBrowser
+  ;
+
+type
+  TCanNavigateEvent =
+    procedure (
+      Sender: TObject;
+      const iURL: String;
+      var ioCanNavigate: Boolean) of object;
+
+  IWebBrowserEx = interface
+  ['{66AB09D6-6B38-49DA-B831-B00F43EAF471}']
+    function GetTagValue(const iTagName, iValueName: String): String;
+    function GetHTMLSource: String;
+  end;
+
+  IWebBrowserCanNavigate = interface
+  ['{04EE8067-DEBD-425F-A407-19A473C82049}']
+    function CanNavigate(const iURL: String): Boolean;
+  end;
+
+  [
+    ComponentPlatformsAttribute(
+      pidWin32
+      or pidWin64
+      or pidOSX32
+      or pidiOSSimulator
+      or pidiOSDevice
+      or pidAndroid
+    )
+  ]
+  TWebBrowserEx = class(TWebBrowser, IWebBrowserCanNavigate)
+  private
+    FOnCanNavigate: TCanNavigateEvent;
+    procedure ParentResize(Sender: TObject);
+    function GetHTMLSource: String;
+    function CanNavigate(const iURL: String): Boolean;
+  protected
+    procedure SetParent(const Value: TFmxObject); override;
+    procedure SetVisible(const Value: Boolean); override;
+    procedure RaiseNotSupport(const iMsg: String);
+  public
+    constructor Create(Owner: TComponent); override;
+    destructor Destroy; override;
+    procedure CheckBounds;
+    function GetWeb: ICustomBrowser;
+    procedure EvaluateJavaScript(const JavaScript: string);
+    procedure CallJS(
+      const iFunction: String;
+      const iParams: array of String); overload;
+    procedure CallJS(const iFunction: String); overload;
+    function GetTagValue(const iTagName, iValueName: String): String;
+    property HTMLSource: String read GetHTMLSource;
+    property OnCanNavigate: TCanNavigateEvent
+      read FOnCanNavigate write FOnCanNavigate;
+  end;
+
+implementation
+
+uses
+  System.Rtti, System.SysUtils
+  , FMX.Controls
+  {$IFDEF MSWINDOWS}
+    , FMX.WebBrowser.Win
+  {$ENDIF}
+  {$IF defined(MACOS) and not defined(IOS)}
+    , FMX.WebBrowser.Cocoa
+  {$ENDIF}
+  ;
+
+{ TWebBrowserEx }
+
+procedure TWebBrowserEx.CallJS(const iFunction: String);
+begin
+  CallJS(iFunction, []);
+end;
+
+function TWebBrowserEx.CanNavigate(const iURL: String): Boolean;
+begin
+  Result := True;
+
+  if (Assigned(FOnCanNavigate)) then
+    FOnCanNavigate(Self, iURL, Result);
+end;
+
+procedure TWebBrowserEx.CallJS(
+  const iFunction: String;
+  const iParams: array of String);
+var
+  Params: TStringBuilder;
+  Param: String;
+begin
+  Params := TStringBuilder.Create;
+  try
+    for Param in iParams do begin
+      Params.Append(',');
+      Params.Append(Param);
+    end;
+
+    if (Params.Length > 0) then
+      Params.Remove(0, 1);
+
+    EvaluateJavaScript(Format('%s(%s);', [iFunction, Params.ToString]));
+  finally
+    Params.Free;
+  end;
+end;
+
+function TWebBrowserEx.GetHTMLSource: String;
+var
+  Web: ICustomBrowser;
+  WebEx: IWebBrowserEx;
+begin
+  Web := GetWeb;
+
+  if (Web = nil) then
+    Exit;
+
+  if (Supports(Web, IWebBrowserEx, WebEx)) then
+    Result := WebEx.GetHTMLSource
+  else
+    RaiseNotSupport('GetHTML method');
+end;
+
+function TWebBrowserEx.GetTagValue(const iTagName, iValueName: String): String;
+var
+  Web: ICustomBrowser;
+  WebEx: IWebBrowserEx;
+begin
+  Web := GetWeb;
+
+  if (Web = nil) then
+    Exit;
+
+  if (Supports(Web, IWebBrowserEx, WebEx)) then
+    Result := WebEx.GetTagValue(iTagname, iValueName)
+  else
+    RaiseNotSupport('GetTagValue method');
+end;
+
+procedure TWebBrowserEx.CheckBounds;
+var
+  Web: ICustomBrowser;
+begin
+  Web := GetWeb;
+
+  if (Web <> nil) then
+    Web.UpdateContentFromControl;
+end;
+
+constructor TWebBrowserEx.Create(Owner: TComponent);
+begin
+  inherited;
+
+  HitTest := False;
+  TabStop := False;
+end;
+
+destructor TWebBrowserEx.Destroy;
+var
+  Web: ICustomBrowser;
+begin
+  Web := GetWeb;
+
+  inherited;
+
+  if (Web <> nil)  then
+    Web.Hide;
+end;
+
+procedure TWebBrowserEx.EvaluateJavaScript(const JavaScript: string);
+var
+  Web: ICustomBrowser;
+begin
+  Web := GetWeb;
+
+  if (Web <> nil) then
+    Web.EvaluateJavaScript(JavaScript);
+end;
+
+function TWebBrowserEx.GetWeb: ICustomBrowser;
+var
+  RttiType: TRttiType;
+  Web: ICustomBrowser;
+begin
+  RttiType := SharedContext.GetType(TWebBrowser);
+  Web := ICustomBrowser(RttiType.GetField('FWeb').GetValue(Self).AsInterface);
+
+  Result := Web;
+end;
+
+procedure TWebBrowserEx.ParentResize(Sender: TObject);
+begin
+  CheckBounds;
+end;
+
+procedure TWebBrowserEx.RaiseNotSupport(const iMsg: String);
+begin
+  raise
+    ENotSupportedException.Create(iMsg + ' is not supported on this platform.');
+end;
+
+procedure TWebBrowserEx.SetParent(const Value: TFmxObject);
+var
+  Web: ICustomBrowser;
+begin
+  if (Parent <> nil) and (Parent is TControl) then
+    TControl(Parent).OnResize := nil;
+
+  inherited;
+
+  if (Parent <> nil) then begin
+    if (Parent is TControl) then
+      TControl(Parent).OnResize := ParentResize;
+
+    Web := GetWeb;
+    if (Web <> nil) then
+      Web.SetWebBrowserControl(Self);
+
+    Show;
+  end;
+end;
+
+procedure TWebBrowserEx.SetVisible(const Value: Boolean);
+var
+  Web: ICustomBrowser;
+begin
+  inherited;
+
+  Web := GetWeb;
+
+  if (Web <> nil) then begin
+    if (Value) then
+      Web.Show
+    else
+      Web.Hide;
+  end;
+end;
+
+// XE8 以降は Win/OSX にも TWebBrowser が提供されている
+// クラス名が被って実行時エラーになるのを防ぐ
+{$IF RTLVersion < 29}
+initialization
+  RegisterFmxClasses([TWebBrowser]);
+{$ENDIF}
+
+end.
---
+++ main.pas
@@ -0,0 +1,231 @@
+﻿unit main;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Edit,
+  FMX.ComboEdit, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Ani, FMX.Maps,
+  FMX.WebBrowser, IPPeerClient, REST.Client, Data.Bind.Components,
+  Data.Bind.ObjectScope, FMX.Layouts, FMX.ListBox, System.JSON,
+  FMX.ListView.Types, FMX.ListView.Appearances, FMX.ListView.Adapters.Base,
+  FMX.ListView, FMX.SearchBox, Data.Bind.GenData, System.Rtti,
+  System.Bindings.Outputs, Fmx.Bind.Editors, Data.Bind.EngExt, Fmx.Bind.DBEngExt,
+  System.Generics.Collections, Data.Bind.Controls,
+  Fmx.Bind.Navigator, IdURI, FMX.TabControl, System.Actions, FMX.ActnList,
+  System.Sensors, System.Sensors.Components;
+
+type
+  TForm1 = class(TForm)
+    RESTRequest1: TRESTRequest;
+    RESTClient1: TRESTClient;
+    RESTResponse1: TRESTResponse;
+    onChangeEditTextTimer: TTimer;
+    ToolBar1: TToolBar;
+    Edit1: TEdit;
+    ClearEditButton1: TClearEditButton;
+    ListView1: TListView;
+    TabControl1: TTabControl;
+    Settings: TTabItem;
+    Weather: TTabItem;
+    TabControl2: TTabControl;
+    TabItem1: TTabItem;
+    NearBy: TTabItem;
+    ActionList1: TActionList;
+    gotoWeather: TChangeTabAction;
+    gotoSettings: TChangeTabAction;
+    ToolBar2: TToolBar;
+    Label1: TLabel;
+    WebBrowser1: TWebBrowser;
+    SpeedButton1: TSpeedButton;
+    AniIndicator1: TAniIndicator;
+    ToolBar3: TToolBar;
+    Label2: TLabel;
+    LocationSensor1: TLocationSensor;
+    ListView2: TListView;
+    onLocationChanged: TTimer;
+    AniIndicator2: TAniIndicator;
+    Label3: TLabel;
+    ListBox1: TListBox;
+    ListBoxItem1: TListBoxItem;
+    AniIndicator3: TAniIndicator;
+    gotoNearBy: TChangeTabAction;
+    procedure Edit1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure SpeedButton1Click(Sender: TObject);
+    procedure ListView1ItemClick(const Sender: TObject;
+      const AItem: TListViewItem);
+    procedure WebBrowser1DidFinishLoad(ASender: TObject);
+    procedure LocationSensor1LocationChanged(Sender: TObject; const OldLocation,
+      NewLocation: TLocationCoord2D);
+    procedure NearByClick(Sender: TObject);
+    procedure onLocationChangedTimer(Sender: TObject);
+    procedure onChangeEditTextTimerTimer(Sender: TObject);
+    procedure ListView2ItemClick(const Sender: TObject;
+      const AItem: TListViewItem);
+  private
+    goFromTab: string;
+    oldSearchBoxText: string;
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+{$R *.SmXhdpiPh.fmx ANDROID}
+{$R *.LgXhdpiPh.fmx ANDROID}
+{$R *.XLgXhdpiTb.fmx ANDROID}
+{$R *.NmXhdpiPh.fmx ANDROID}
+{$R *.LgXhdpiTb.fmx ANDROID}
+
+procedure setRESTRequestParam(name, value: string);
+begin
+  try
+    Form1.RESTRequest1.Params.ParameterByName(name).Value := value;
+  except
+    Form1.RESTRequest1.AddParameter(name, value);
+  end;
+  Form1.RESTRequest1.AddParameter(name, value);
+end;
+
+procedure getCityData(ListView: TListView);
+var jValue: TJSONValue;
+    jArray: TJSONArray;
+    json, json1: TJSONObject;
+    ListViewItem: TListViewItem;
+    region, municipal: string;
+    function getStr(json: TJSONObject; value: string): string;
+    begin
+      try
+         Result := json.Get(value).JsonValue.value;
+      except
+         Result := '';
+      end;
+    end;
+begin
+  with Form1 do begin
+    RESTRequest1.Execute;
+    ListView.Items.Clear;
+    AniIndicator2.Visible := false;
+    AniIndicator3.Visible := false;
+    jValue:=RESTResponse1.JSONValue;
+    if (jValue is TJSONObject) then begin
+      json :=  TJSONObject(jValue);
+      jArray := json.GetValue('items') as TJSONArray;
+      for jValue in jArray do
+      begin
+        json1 :=  TJSONObject(jValue);
+        ListViewItem := ListView.Items.Add;
+        region := getStr(json1, 'd_name');
+        if (region<>'') then  region := ', '+region;
+        municipal := getStr(json1, 'mun_name');
+        if (municipal<>'') then  municipal := ', '+municipal;
+        ListViewItem.Detail := getStr(json1, 'c_name') + region + municipal;
+        ListViewItem.Text := json1.Get('name').JsonValue.value;
+        ListViewItem.Tag := strToIntDef(getStr(json1, 'id'), 0);
+      end;
+    end;
+  end;
+end;
+
+procedure TForm1.Edit1Click(Sender: TObject);
+begin
+  edit1.Text := '';
+  edit1.FontColor := TAlphaColorRec.Black;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  oldSearchBoxText := 'Поиск погоды';
+  edit1.Text := oldSearchBoxText;
+  edit1.FontColor:= TAlphaColorRec.Darkgray;
+end;
+
+procedure TForm1.ListView1ItemClick(const Sender: TObject;
+  const AItem: TListViewItem);
+begin
+  goFromTab := 'search';
+  WebBrowser1.Visible := false;
+  AniIndicator1.Visible := true;
+  gotoWeather.ExecuteTarget(self);
+  WebBrowser1.URL := 'http://m.meteonova.ru/frc/'+intToStr(AItem.Tag)+'.html';
+  //WebBrowser1.EvaluateJavaScript('renderMarker('+intToStr(AItem.Tag)+');');
+end;
+
+procedure TForm1.ListView2ItemClick(const Sender: TObject;
+  const AItem: TListViewItem);
+begin
+  goFromTab := 'nearby';
+  WebBrowser1.Visible := false;
+  AniIndicator1.Visible := true;
+  gotoWeather.ExecuteTarget(self);
+  WebBrowser1.URL := 'http://m.meteonova.ru/frc/'+intToStr(AItem.Tag)+'.html';
+  //WebBrowser1.EvaluateJavaScript('renderMarker('+intToStr(AItem.Tag)+');');
+end;
+
+procedure TForm1.LocationSensor1LocationChanged(Sender: TObject;
+  const OldLocation, NewLocation: TLocationCoord2D);
+var LDecSeparator: Char;
+    LSettings: TFormatSettings;
+    URLString: string;
+begin
+  //LDecSeparator := FormatSettings.DecimalSeparator;
+  //LSettings := FormatSettings;
+  try
+    //FormatSettings.DecimalSeparator := '.';
+    RESTRequest1.Params.Clear;
+    setRESTRequestParam('searchby', 'cities');
+    setRESTRequestParam('lat', Format('%2.6f', [NewLocation.Latitude]));
+    setRESTRequestParam('lng', Format('%2.6f', [NewLocation.Longitude]));
+    LocationSensor1.Active := false;
+    onLocationChanged.Enabled :=true;
+  finally
+    //FormatSettings.DecimalSeparator := LDecSeparator;
+  end;
+end;
+
+procedure TForm1.onChangeEditTextTimerTimer(Sender: TObject);
+begin
+    if  (oldSearchBoxText<>edit1.Text) then
+    begin
+      oldSearchBoxText := edit1.Text;
+      if edit1.Text = '' then exit;
+      AniIndicator3.Visible := true;
+      setRESTRequestParam('fchar', edit1.Text);
+      setRESTRequestParam('mcntcities', '30');
+      getCityData(ListView1);
+    end;
+end;
+
+procedure TForm1.onLocationChangedTimer(Sender: TObject);
+begin
+  getCityData(ListView2);
+  onLocationChanged.Enabled := false;
+end;
+
+procedure TForm1.SpeedButton1Click(Sender: TObject);
+begin
+  if goFromTab = 'search' then
+    gotoSettings.ExecuteTarget(self)
+  else
+    gotoNearBy.ExecuteTarget(self);
+end;
+
+procedure TForm1.NearByClick(Sender: TObject);
+begin
+  AniIndicator2.Visible := true;
+  LocationSensor1.Active := true;
+end;
+
+procedure TForm1.WebBrowser1DidFinishLoad(ASender: TObject);
+begin
+  AniIndicator1.Visible := false;
+  WebBrowser1.Visible := true;
+end;
+
+end.
